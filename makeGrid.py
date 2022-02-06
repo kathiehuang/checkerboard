fdb08fece6574e0b9e85c96c8948dddd8e8e8fe1
Connie Xu <cx71@duke.edu>
2022-02-06 16:27:56 -0500

    saves image, returns it in makeGrid

--- makeGrid.py
+++ makeGrid.py
@@ -11,11 +11,12 @@ practiceInput = np.array([[1,0,1,0,1,0],
 # takes in 6x6 numpy array, returns array for now 
 
 def makeGrid(arr):
-    plt.figure(figsize=(12,12))
+    plt.figure(figsize=(5,5))
     plt.imshow(arr.squeeze(), cmap = 'gray')
     plt.axis(False)
+    plt.savefig('apriltag.png', dpi=300, bbox_inches='tight', pad_inches = 0)
     plt.show()
-    return arr
+    return 'apriltag.png'
 
 #add black border 
 practiceInput = np.insert(practiceInput, 0, [0,0,0,0,0,0], axis = 0)
@@ -28,6 +29,7 @@ practiceInput = np.insert(practiceInput, 0, [1,1,1,1,1,1,1,1], axis = 0)
 practiceInput = np.insert(practiceInput, len(practiceInput), [1,1,1,1,1,1,1,1], axis = 0)
 practiceInput = np.insert(practiceInput, 0, 1, axis = 1)
 practiceInput = np.insert(practiceInput, len(practiceInput[0]), 1, axis = 1)
-print(practiceInput)
+
+#print(practiceInput)
 makeGrid(practiceInput)
 
